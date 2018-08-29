5b829c96b50d52593bffeff9ced2487f9fa560bc
Seth Walton <sdwalton83@gmail.com>
2018-08-29 15:30:32 -0500

    add new places with longpress

--- MemorablePlaces/ViewController.swift
+++ MemorablePlaces/ViewController.swift
@@ -14,9 +14,15 @@ class ViewController: UIViewController,MKMapViewDelegate {
     @IBOutlet var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
         
-        if activePlace != -1 {
+        uilpgr.minimumPressDuration = 2
+        map.addGestureRecognizer(uilpgr)
+        
+        if activePlace == -1 {
             // get place details to display on map
+            
+        }else {
             
             if places.count > activePlace{
                 
@@ -50,6 +56,49 @@ class ViewController: UIViewController,MKMapViewDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @objc func longpress(gestureRecognizer: UIGestureRecognizer){
+        if gestureRecognizer.state == UIGestureRecognizerState.began{
+            
+            let touchPoint = gestureRecognizer.location(in: self.map)
+            let newCoordinate = self.map.convert(touchPoint, toCoordinateFrom: self.map)
+            
+            
+            let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            var title = ""
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: {(placemarks, error)
+                in
+                
+                if error != nil {
+                    print(error)
+                } else {
+                    if let placemark = placemarks?[0]{
+                        if placemark.subThoroughfare != nil {
+                            
+                            title += placemark.subThoroughfare! + " "
+                        }
+                        if placemark.thoroughfare != nil {
+                            title += placemark.thoroughfare!
+                        }
+                    }
+                }
+                
+                if title == "" {
+                    title = "Added \(NSDate())"
+                }
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = newCoordinate
+                annotation.title = title
+                self.map.addAnnotation(annotation)
+                
+                places.append(["name": title, "lat": String(newCoordinate.latitude), "lon": String(newCoordinate.longitude)])
+                print(places)
+            })
+            
+            
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
